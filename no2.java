f7e83e5ebf9502c5fc1e2d5ffebb253a0f2cfaa7
Rahmadi <147220595+Rahmadiilyas@users.noreply.github.com>
2024-03-21 14:52:12 +0800

    no2.java

--- no2.java
+++ no2.java
@@ -1,4 +1,4 @@
-
+package praktikum3;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -8,46 +8,18 @@ public class no2 {
 
     public static void main(String[] args) {
         Queue<Double> data = new LinkedList<>();
-
-        data.offer(91.35);
-        data.offer(4.72);
-        data.offer(26.0);
-        data.offer(3.5);
-        data.offer(8.0);
-        data.offer(61.0);
-        data.offer(65.36);
-        data.offer(12.0);
-        data.offer(5.6);
-        data.offer(7.11);
-        data.offer(27.0);
-        data.offer(9.53);
-        data.offer(549.0);
-        data.offer(2.23);
-        data.offer(4.25);
-        data.offer(2.13);
-        data.offer(83.0);
-        data.offer(7.0);
-        data.offer(102.4);
-        data.offer(53.21);
-        data.offer(3.42);
-        data.offer(0.21);
-        data.offer(70.02);
-        data.offer(819.4);
-        data.offer(6173.0);
-        data.offer(4.25);
-        data.offer(19.8);
-        data.offer(17.35);
-        data.offer(5.768);
+        double[] nilai = {91.35, 4.72, 26.0, 3.5, 8.0, 61.0, 65.36, 12.0, 5.6, 7.11,
+            27.0, 9.53, 549.0, 2.23, 17.31, 4.25, 2.13, 83.0, 7.0, 102.4,
+            53.21, 3.42, 0.21, 70.02, 819.4, 6173.0, 4.25, 19.8, 17.35, 5.768};
 
         double min = Double.MAX_VALUE;
         double max = Double.MIN_VALUE;
         double sum = 0;
-       
-        int size = data.size(); 
+        for (int i = 0; i < nilai.length; i++) {
+            data.offer(nilai[i]);
 
-        while (!data.isEmpty()) {
-            double value = data.poll();
-           
+        }
+        for (double value : data) {
             if (value < min) {
                 min = value;
             }
@@ -55,9 +27,9 @@ public class no2 {
                 max = value;
             }
             sum += value;
-        }
 
-        double average = sum / size; 
+        }
+        double average = sum / data.size();
 
         System.out.println("Min     : " + min);
         System.out.println("Max     : " + max);
